185df076f8b88e738cb42d024c8f8686a9e947d2
ralf.ovelgoenne@gmail.com <ralf.ovelgoenne@gmail.com@0c7aaf37-e6e4-4a69-49d7-a1a03881413e>
2012-07-16 14:21:34 +0000

    Added OpenSearch provider implementation

---
+++ trunk/GSAOneBoxProvider/src/com/qperior/gsa/oneboxprovider/implementations/opensearch/QPOpenSearchProvider.java
@@ -0,0 +1,21 @@
+package com.qperior.gsa.oneboxprovider.implementations.opensearch;
+
+import com.qperior.gsa.oneboxprovider.provider.QPAbstractProvider;
+import com.qperior.gsa.oneboxprovider.results.QPIOneBoxResults;
+import com.qperior.gsa.oneboxprovider.util.exception.QPProviderException;
+
+public class QPOpenSearchProvider extends QPAbstractProvider {
+
+	@Override
+	public QPIOneBoxResults provideOneBoxResults() throws QPProviderException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProviderName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ trunk/GSAOneBoxProvider/src/com/qperior/gsa/oneboxprovider/implementations/opensearch/QPOpenSearchSecurityProvider.java
@@ -0,0 +1,20 @@
+package com.qperior.gsa.oneboxprovider.implementations.opensearch;
+
+import java.util.List;
+
+import com.qperior.gsa.oneboxprovider.provider.QPAbstractProvider;
+import com.qperior.gsa.oneboxprovider.results.QPIOneBoxResults;
+import com.qperior.gsa.oneboxprovider.security.QPAbstractSecurityProvider;
+import com.qperior.gsa.oneboxprovider.security.QPAuthType;
+import com.qperior.gsa.oneboxprovider.util.exception.QPProviderException;
+
+public class QPOpenSearchSecurityProvider extends QPAbstractSecurityProvider {
+
+	@Override
+	public List<QPAuthType> getSupportedAuthTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
